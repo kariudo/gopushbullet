5eff3afa3f740cabb67c95f534131e10d5bb58b3
Hunter Horsman <kariudo@gmail.com>
2015-01-30 23:47:21 -0500

    Add contact, subscription, and channel structs

--- gopushbullet.go
+++ gopushbullet.go
@@ -11,10 +11,10 @@ type ErrorResponse struct {
 
 //PushMessage describes a message to be sent via PushBullet. Only one of the first 4 properties may be specified with a message being sent.
 type PushMessage struct {
-	DeviceIdentity string `json:"device_iden"`
-	Email          string `json:"email"`
-	ChannelTag     string `json:"channel_tag"`
-	ClientIdentity string `json:"client_iden"`
+	DeviceID   string `json:"device_iden"`
+	Email      string `json:"email"`
+	ChannelTag string `json:"channel_tag"`
+	ClientID   string `json:"client_iden"`
 
 	// Type indicates the type of push message being sent
 	Type    string   `json:"type"`    // note, link, address, checklist, or file
@@ -24,15 +24,15 @@ type PushMessage struct {
 	Address string   `json:"address"` // Address is used with address type
 	Items   []string `json:"items"`   // Items are used with checklist types
 	// The following are used with file types
-	FileName             string `json:"file_name"`
-	FileType             string `json:"file_type"` // MIME type of the file
-	FileURL              string `json:"file_url"`
-	SourceDeviceIdentity string `json:"source_device_iden"`
+	FileName       string `json:"file_name"`
+	FileType       string `json:"file_type"` // MIME type of the file
+	FileURL        string `json:"file_url"`
+	SourceDeviceID string `json:"source_device_iden"`
 }
 
-//Device describes a registered device (phone, stream)
+//Device describes a registered device (phone, stream).
 type Device struct {
-	Identity     string  `json:"iden"`
+	ID           string  `json:"iden"`
 	PushToken    string  `json:"push_token"`
 	AppVersion   int     `json:"app_version"`
 	Fingerprint  string  `json:"fingerprint"`
@@ -44,4 +44,33 @@ type Device struct {
 	Modified     float32 `json:"modified"`
 	Model        string  `json:"model"`
 	Pushable     bool    `json:"pushable"`
+}
+
+//Contact describes a contact entry.
+type Contact struct {
+	ID              string  `json:"iden"`
+	Name            string  `json:"name"`
+	Created         float32 `json:"created"`
+	Modified        float32 `json:"modified"`
+	Email           string  `json:"email"`
+	EmailNormalized string  `json:"email_normalized"`
+	Active          bool    `json:"active"`
+}
+
+//Subscription describes a channel subscription.
+type Subscription struct {
+	ID       string  `json:"iden"`
+	Created  float32 `json:"created"`
+	Modified float32 `json:"modified"`
+	Active   bool    `json:"active"`
+	Channel  Channel `json:"channel"`
+}
+
+//Channel describes a channel on a subscription.
+type Channel struct {
+	ID          string `json:"iden"`
+	Tag         string `json:"tag"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	ImageURL    string `json:"image_url"`
 }
